5fafd02f2f0efc16b119aaec4d6936b936b87591
Parvez Khan <iamparvezkhan25@gmail.com>
2024-10-09 14:56:00 +0530

    both filed tests and hello changes has been done

--- hello.js
+++ hello.js
@@ -6,12 +6,17 @@ console.log("inception feature");
 console.log('feature begin');
 console.log('feature mid');
 console.log('feature end');
+console.log('after feature');
 
 
 // now i am solving this major feature
 console.log('inception feature');
-console.log("Solving mejor bug!");
+console.log("Solving major bug!");
 console.log("Solving minor bug!");
+console.log("After Solving major bug!");
 
 //task 2 i working on
 console.log("task 2");
+
+//task 2 ammend
+console.log("task 2.1");
