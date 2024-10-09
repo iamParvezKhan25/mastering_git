7a00599502c8806046149437c2c30f0b5d73e693
Parvez Khan <iamparvezkhan25@gmail.com>
2024-10-09 13:38:39 +0530

    feature 1 and task 2 and solving major minor bug in hello.js togather

--- hello.js
+++ hello.js
@@ -1,6 +1,15 @@
 console.log("Hello Git!");
 console.log("Hello GitHub!");
 
+// Implement IMP feature
+console.log('feature begin');
+console.log('feature mid');
+console.log('feature end');
+
+
 // now i am solving this major feature
 console.log("Solving mejor bug!");
 console.log("Solving minor bug!");
+
+//task 2 i working on
+console.log("task 2");
